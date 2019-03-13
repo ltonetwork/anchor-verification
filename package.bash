d0b8e3f86fe0ff9d79702ae620ea97a49fef986b
KillaBee1985 <y.horbachov@gmail.com>
2019-03-13 17:04:18 +0100

    Add .js file

--- package.bash
+++ package.bash
@@ -2,4 +2,5 @@
 rm -rf docs/lto-anchor-verification
 mkdir docs/lto-anchor-verification
 cat dist/lto-anchor-verification/{runtime,polyfills,scripts,main}.js | gzip > docs/lto-anchor-verification/lto-anchor-verification.js.gz
+cat dist/lto-anchor-verification/{runtime,polyfills,scripts,main}.js > docs/lto-anchor-verification/lto-anchor-verification.js
 cp dist/lto-anchor-verification/styles.css docs/lto-anchor-verification/lto-anchor-verification.css
