3654541b14520baf31d1fe714b22c50fcfd5be85
Arnold Daniels <arnold@jasny.net>
2022-01-17 20:25:05 +0100

    Fixup

--- package.bash
+++ package.bash
@@ -4,3 +4,4 @@ mkdir docs/lto-anchor-verification
 cat dist/lto-anchor-verification/{runtime,polyfills,scripts,main}.js | gzip > docs/lto-anchor-verification/lto-anchor-verification.js.gz
 cat dist/lto-anchor-verification/{runtime,polyfills,scripts,main}.js > docs/lto-anchor-verification/lto-anchor-verification.js
 cp dist/lto-anchor-verification/styles.css docs/lto-anchor-verification/lto-anchor-verification.css
+
